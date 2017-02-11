0a81bf4d98d21cea5621afb1a24d666d3f43b35d
indradenbakker <indra.den.bakker@gmail.com>
2017-02-11 16:49:21 +0100

    Updated calibrate_camera.py
    Calibration automatically saved to file

--- algorithms/calibrate_camera.py
+++ algorithms/calibrate_camera.py
@@ -3,50 +3,47 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+import glob
+
+import pickle
+
+
 def compute_calibration(img, objpoints, imgpoints):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     shape = gray.shape[::-1]
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, shape,None,None)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, shape, None, None)
     undist = cv2.undistort(img, mtx, dist, None, mtx)
     return(undist, mtx, dist)
 
-def undistort_and_warp(fname, dim=(9,6)):
+def undistort_and_warp(directory, dim=(9,6)):
 
     objp = np.zeros((6*9,3), np.float32)
     objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
     objpoints = [] 
     imgpoints = [] 
 
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    img_size = (gray.shape[1], gray.shape[0])
+    cam_images = glob.glob(directory)
 
-    ret, corners = cv2.findChessboardCorners(gray, dim, None)
+    for fname in cam_images:
+        print(fname)
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        img_size = (gray.shape[1], gray.shape[0])
 
-    # If not found, return
-    if ret == False:
-        print("No chessboard corners found")
-        return(img, None, None)
+        ret, corners = cv2.findChessboardCorners(gray, dim, None)
 
-    objpoints.append(objp)
-    imgpoints.append(corners)
+        # If not found, return
+        if ret == False:
+            print("No chessboard corners found")
+        else:         
+            objpoints.append(objp)
+            imgpoints.append(corners)
 
-    img = cv2.drawChessboardCorners(img, dim, corners, ret)
-    
-    offset = 100
+    shape = gray.shape[::-1]
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, shape, None, None)
 
-    src = np.float32([corners[0], corners[dim[0]-1], corners[-1], corners[-dim[0]]])
-    dst = np.float32([[offset, offset], [img_size[0]-offset, offset], [img_size[0]-offset, img_size[1]-offset], [offset, img_size[1]-offset]])
-    
-    M = cv2.getPerspectiveTransform(src, dst)
-    
-    img_undist, mtx, distortion_coef = compute_calibration(img, objpoints, imgpoints)
-    warped = cv2.warpPerspective(img_undist, M, img_size)
-    f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 10))
-    ax1.imshow(img)
-    ax1.set_title('Original Image')
-    ax2.imshow(img_undist)
-    ax2.set_title('Undistorted Image')
-    ax3.imshow(warped)
-    ax3.set_title('Undistorted & Warped Image')
-    return(img_undist, mtx, distortion_coef)
+    save_dict = {'mtx': mtx, 'dist': dist}
+    with open('calibrate_camera.p', 'wb') as f:
+        pickle.dump(save_dict, f)
+
+    return(mtx, dist)
